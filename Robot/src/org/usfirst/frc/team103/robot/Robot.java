9b9c43bcaecd1621819135250237325e28bf4b29
Cybersonics <cybersonics@palisadessd.org>
2015-02-05 14:53:44 -0500

    Made the init report errors without errors

--- Robot/src/org/usfirst/frc/team103/robot/Robot.java
+++ Robot/src/org/usfirst/frc/team103/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.CameraServer;
 
@@ -24,80 +25,100 @@ public class Robot extends SampleRobot {
 	DoubleSolenoid s0, s1;
     RobotDrive drive;
     Joystick controller;
+    Joystick stick1;
+    Joystick stick2;
+    Joystick stick3;
     JoystickButton bButton;
     Compressor compressor;
     CameraServer server;
 
     public Robot() {
-    	talon0 = new CANTalon(0);
-    	talon1 = new CANTalon(1);
-    	talon2 = new CANTalon(2);
-    	talon3 = new CANTalon(3);
-    	talon0.changeControlMode(ControlMode.PercentVbus);
-    	talon1.changeControlMode(ControlMode.Follower);
-    	talon2.changeControlMode(ControlMode.Follower);
-    	talon3.changeControlMode(ControlMode.PercentVbus);
-    	talon1.set(0);
-    	talon2.set(3);
-    	
-    	s0 = new DoubleSolenoid(0,1);
-    	s1 = new DoubleSolenoid(2,3);
-    	
-        drive = new RobotDrive(talon0, talon3);
-        drive.setSafetyEnabled(true);
-        drive.setExpiration(0.1);
-
-        tower = new Talon(0);
-        tower.setSafetyEnabled(true);
-        tower.setExpiration(0.1);
-        
-        pot = new AnalogPotentiometer(0);
-        
-        controller = new Joystick(0);
-        bButton = new JoystickButton(controller, 2);
-        bButton.whenPressed(new Command() {
-        	private boolean isOpen = true;
-			@Override
-			protected boolean isFinished() { return true; }
-			@Override
-			protected void interrupted() { }
-			@Override
-			protected void initialize() {
-				if (Robot.this.isOperatorControl()) {
-					s0.set((isOpen = !isOpen) ? DoubleSolenoid.Value.kReverse : DoubleSolenoid.Value.kForward);
-				}
-			}
-			@Override
-			protected void execute() { }
-			@Override
-			protected void end() { }
-		});
-        
-        compressor.start();
-        SmartDashboard.putBoolean("pcm", compressor.getClosedLoopControl());
-        
-        server = CameraServer.getInstance();
-        server.setQuality(50);
-        server.startAutomaticCapture("cam0");
+    	try{
+    		talon0 = new CANTalon(0);
+    		talon1 = new CANTalon(1);
+    		talon2 = new CANTalon(2);
+    		talon3 = new CANTalon(3);
+    		talon0.changeControlMode(ControlMode.PercentVbus);
+    		talon1.changeControlMode(ControlMode.Follower);
+    		talon2.changeControlMode(ControlMode.Follower);
+    		talon3.changeControlMode(ControlMode.PercentVbus);
+    		talon1.set(0);
+    		talon2.set(3);
+    		
+    		s0 = new DoubleSolenoid(0,1);
+    		s1 = new DoubleSolenoid(2,3);
+    		
+    		drive = new RobotDrive(talon0, talon3);
+    		drive.setSafetyEnabled(true);
+    		drive.setExpiration(0.1);
+    		
+    		tower = new Talon(0);
+    		tower.setSafetyEnabled(true);
+    		tower.setExpiration(0.1);
+    		
+    		pot = new AnalogPotentiometer(0);
+    		
+    		//controller = new Joystick(0);
+    		stick1 = new Joystick(0);
+    		stick2 = new Joystick(1);
+    		stick3 = new Joystick(2);
+    		//bButton = new JoystickButton(controller, 2);
+    		bButton = new JoystickButton(stick3, 2);
+    		bButton.whenPressed(new Command() {
+    			private boolean isOpen = true;
+    			@Override
+    			protected boolean isFinished() { return true; }
+    			@Override
+    			protected void interrupted() { }
+    			@Override
+    			protected void initialize() {
+    				SmartDashboard.putBoolean("open state", isOpen);
+    				if (Robot.this.isOperatorControl()) {
+    					s0.set((isOpen = !isOpen) ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse);
+    				}
+    			}
+    			@Override
+    			protected void execute() { }
+    			@Override
+    			protected void end() { }
+    		});	
+    		compressor = new Compressor();
+    		compressor.setClosedLoopControl(true);
+    		SmartDashboard.putBoolean("pcm", compressor.getClosedLoopControl());
+    	}catch(Exception e){
+    		SmartDashboard.putString("Startup Errors", e.getMessage());
+    	}
+        try{
+        	server = CameraServer.getInstance();
+            server.setQuality(50);
+            server.startAutomaticCapture("cam0");
+        }catch(Exception e){
+        	SmartDashboard.putString("Startup Errors", "cam 0 failed to start");
+        }
     }
 
     public void operatorControl() {
+        Scheduler.getInstance().enable();
     	talon0.enableControl();
     	talon1.enableControl();
     	talon2.enableControl();
     	talon3.enableControl();
         while (isOperatorControl() && isEnabled()) {
         	SmartDashboard.putNumber("string pot", pot.get());
-        	double slowMod = controller.getRawButton(6) ? 1.5 : 1.0;
-            drive.tankDrive(-controller.getRawAxis(1) / slowMod, -controller.getRawAxis(5) / slowMod, true);
-            tower.set(controller.getRawAxis(3) - controller.getRawAxis(2));
+        	//double slowMod = controller.getRawButton(6) ? 1.5 : 1.0;
+        	double slowMod = stick1.getRawButton(1) ? 1.5 : 1.0;
+            //drive.tankDrive(-controller.getRawAxis(1) / slowMod, -controller.getRawAxis(5) / slowMod, true);
+            //tower.set(controller.getRawAxis(3) - controller.getRawAxis(2));
+            drive.tankDrive(-stick1.getRawAxis(1) / slowMod, -stick2.getRawAxis(1) / slowMod, true);
+            tower.set(stick3.getRawAxis(1));
+        	Scheduler.getInstance().run();
             Timer.delay(0.005);
-            if(controller.getRawButton(3)){
-            	//s0.set(DoubleSolenoid.Value.kForward);
+            /*if(controller.getRawButton(3)){
+            	s0.set(DoubleSolenoid.Value.kForward);
             }
             if(controller.getRawButton(2)){
-            	
-            }
+            	s0.set(DoubleSolenoid.Value.kReverse);
+            }*/
         }
     }
     
